9f85d327afd1930b384ea84140ee8e99ba31fbd1
Titah Mostafa <etitah-stagiaire@humanbooster.com>
2024-07-18 12:50:44 +0200

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+const Thing = require("./models/Thing");
+
+mongoose
+  .connect(
+    "mongodb+srv://mosk:Titah1988@cluster0.duftf6q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+
+
+
+
+app.post("/api/stuff", (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body,
+  });
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error: error }));
+});
+
+app.get("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
+});
+
+
+module.exports = app;
